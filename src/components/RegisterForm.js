7603e1018fca1483c771368921e1bbe5fea9ae0e
BhupeshB7 <bhupeshkr2912@gmail.com>
2024-05-13 08:18:56 +0530

    update

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -69,7 +69,7 @@ const RegisterForm = () => {
   const fetchSponsorName = async () => {
     try {
       const sponsorResponse = await axios.get(
-        `https://mlm-psi.vercel.app/api/users/getSponsorName/${formData.sponsorId}`
+        `https://mlm-eo5g.onrender.com/api/users/getSponsorName/${formData.sponsorId}`
         // `https://mlm-psi.vercel.app/api/users/getSponsorName/${formData.sponsorId}`
       );
       setSponsorName(sponsorResponse.data.name);
