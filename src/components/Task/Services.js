ac63c1f3714d797cd5dc82b20c1a660825541123
BhupeshB7 <bhupeshkr2912@gmail.com>
2024-07-01 19:45:17 +0530

    update

--- src/components/Task/Services.js
+++ src/components/Task/Services.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   // baseURL: 'http://localhost:5500/api',
   // baseURL: 'https://mlm-v99c.onrender.com/api',
-  baseURL: 'https://piserver-pf9r.onrender.com/api',
+  baseURL: 'https://piserver-fgj3.onrender.com/api',
 });
 
 
